e8d20a6025a6d5ede0506c88c9bdd88ab56e82ad
Jason Ryan <jason.w.ryan9@gmail.com>
2017-09-13 11:57:18 -0500

    Added initial Firebase access.

--- Lab1/scripts/firebaseAccess.js
+++ Lab1/scripts/firebaseAccess.js
@@ -9,3 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var bigOne = document.getElementById('bigOne');
+var dbRef = firebase.database().ref().child('text');
+dbRef.on('value', snap => bigOne.innerText = snap.val());
+
